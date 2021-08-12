46b4d5fde029386a15ca0e3665705280a69aa9de
MagicAngelTG <88583278+MagicAngelTG@users.noreply.github.com>
2021-08-12 17:56:19 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,14 +4,14 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+    START_TEXT = """<b>Hai {}!!</b>
+
+<b>Iam Just A Advance Auto Filter Bot From Kutty Movies😉
 
 Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
 
-For More Details Click Help Button Below..
-@CrazyBotsz
-</i>"""    
+For More Details Click Help Button Below</b>
+"""    
     
     HELP_TEXT = """
 <b><i><u>How To Use Me!?</u></i></b>
